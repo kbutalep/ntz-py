b998756007d8b0e993221dc7f56cdf0ae0007645
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-08-05 10:52:16 -0400

    built out more functions

--- ntz.py
+++ ntz.py
@@ -11,37 +11,65 @@ from sys import argv
 import json
 
 
-#ADD PERSISTENCE LOGIC
-# json load
-#json dump
+#PERSISTENCE FOR JSON
+def load_json():
+    path = 'ntz.json'
+    with open(path, 'r') as f:
+        lists_data = json.load(f)
+    return lists_data
+
+def dump_json(lists_data):
+    path = 'ntz.json'
+    with open(path, 'w') as f:
+        json.dump(path, f)
+
 
 def remember (note):
-    #add json persist
+    lists_data = load_json()
+
     #add remember logic
     print(note)
 
 def forget():
-    #add json persist
+    lists_data = load_json()
     #add forget logic
+    dump_json(lists_data)
     pass
 
 def create():
-    to_do_list = #json loads function
+    to_do_list = load_json()
     if new_list not in to_do_list:
         new_dict = {new_list: []}
-        #json dump(to_do_list)
+        #add new dict to to do lists dict
+        dump_json(to_do_list)
     else:
         print('To Do List already exists. Please edit or create a new list.')
 
 
 def edit():
+    lists_data = load_json()
+    #select list to edit
+    #add to list
+    #remove from list
+    dump_json(lists_data)
     pass
 
 def clear():
+    lists_data = load_json()
+    #select list to clear
+    #confirm clear
+    #clear list
+    dump_json(lists_data)
     pass
 
-def help():
-    pass
+def ntz_help():
+    print('')
+    print('To make a new list use: -c "new list name"\n')
+    print('To remember a list or items use: -r "list name"\n')
+    print('To forget a list use: -f "list name"\n')
+    print('To edit a list use: -e "list name"\n')
+    print('To clear a list use: clear "list name"\n')
+
 
 def main():
     arg = argv[1]
