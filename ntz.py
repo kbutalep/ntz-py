cc2913f589a4087187c66271ff08c42b83748e0a
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-08-05 09:38:35 -0400

    built out outline of program

--- ntz.py
+++ ntz.py
@@ -1,12 +1,66 @@
-# add your code in this file
-import os
+# What's it look like?
+# ntz has four commands.
+# [r]emember
+# [-c] creates or appends to a category
+# [f]orget a note
+# [e]dit a note
+# clear
 
-# main function
-def cli():
-  pass
-  
-def get_args():
-  return os.sys.argv
-  
-# run the main function
-cli()
+import sys
+from sys import argv
+import json
+
+
+#ADD PERSISTENCE LOGIC
+# json load
+#json dump
+
+def remember (note):
+    #add json persist
+    #add remember logic
+    print(note)
+
+def forget():
+    #add json persist
+    #add forget logic
+    pass
+
+def create():
+    to_do_list = #json loads function
+    if new_list not in to_do_list:
+        new_dict = {new_list: []}
+        #json dump(to_do_list)
+    else:
+        print('To Do List already exists. Please edit or create a new list.')
+
+
+def edit():
+    pass
+
+def clear():
+    pass
+
+def help():
+    pass
+
+def main():
+    arg = argv[1]
+    if arg == 'r':
+        remember(argv[2])
+    elif arg == 'f':
+        forget()
+    elif arg == 'c':
+        create(argv[2])
+    elif arg == 'e':
+        edit()
+    elif arg == 'clear':
+        clear()
+    elif arg == 'help':
+        ntz_help()
+    else:
+        print ('Invalid input')
+        ntz_help()
+
+
+
+main()
